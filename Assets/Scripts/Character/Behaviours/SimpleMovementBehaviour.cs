9b6d4c9010fcb2a28e0e3d149a3024ab846c45d4
Betul <betul.s.99@hotma,l.com>
2023-10-10 14:46:25 +0300

    Movement behaviour edited

--- Assets/Scripts/Character/Behaviours/SimpleMovementBehaviour.cs
+++ Assets/Scripts/Character/Behaviours/SimpleMovementBehaviour.cs
@@ -1,34 +1,38 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class SimpleMovementBehaviour : MonoBehaviour
 {
-    private Vector3 position;
-    private float width;
-    private float height;
+    [SerializeField] private CharacterController _characterController = null;
     [SerializeField] private Transform _characterTransform;
+    [SerializeField] private float _xSpeed = 2f;
+
+    //Todo: Remove that referances from managers
+    [SerializeField] private bool _isGameEnded;
     [SerializeField] private Transform _upBoundry;
     [SerializeField] private Transform _downBoundry;
-    [SerializeField] private CharacterController _characterController = null;
 
-    [SerializeField] private float _xSpeed = 2f;
-    [SerializeField] private float _zSpeed = 2.0f;
-    [SerializeField] private float _ySpeed = -1f;
-    [SerializeField] private bool _isGameEnded;
-    public float moveSpeed = 5.0f;  // Karakterin yatay hareket hýzý
-    public float verticalSpeed = 2.0f;  // Fare yönünde yukarý/aþaðý hareket hýzý
-    public float minY = -2.0f;  // Y ekseninin minimum sýnýrý
-    public float maxY = 2.0f;
-
-    public void Move(float swipeAmount)
+    private void Update()
     {
-        var characterPosition = _characterTransform.position;
-        Vector3 sideWayDir = _characterTransform.right * (swipeAmount - characterPosition.y);
+        if (TryMove())
+        {
+            Move();
+        }
 
-        var direction = _characterTransform.forward * _zSpeed +
-                            sideWayDir * _xSpeed +
-                            new Vector3(0, 0, 0);
-        _characterController.Move(direction /*Vector3.forward * _xSpeed*/ * Time.deltaTime);
+    }
+    private void LateUpdate()
+    {
+        KeepOnPlatform();
+    }
+
+    public void Move()
+    {
+        _characterController.Move(Vector3.right * _xSpeed * Time.deltaTime);
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0f;
+            _characterTransform.position = new Vector3(_characterTransform.position.x, mouseWorldPos.y, 0);
+        }
     }
 
     private bool TryMove()
@@ -40,25 +44,6 @@ public class SimpleMovementBehaviour : MonoBehaviour
         return false;
     }
 
-    private void Update()
-    {
-        if (TryMove())
-        {
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                mouseWorldPos.z = 0f;
-                _characterTransform.position = new Vector3(_characterTransform.position.x, mouseWorldPos.y, 0);
-                Debug.Log("Mouse Pos: " + mouseWorldPos);
-
-            }
-        }
-
-    }
-    private void LateUpdate()
-    {
-        KeepOnPlatform();
-    }
     private void KeepOnPlatform()
     {
         var characterPosition = _characterTransform.position;
